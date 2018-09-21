1115b58274b719d18d9fc692ce231883079534e8
Giorgi <lantareg@gmail.com>
2018-09-20 21:46:03 -0400

    integrated js map

--- src/components/home.vue
+++ src/components/home.vue
@@ -9,7 +9,11 @@
 
         <div class="row flex-fill d-flex">
             <div class="col-12">
-                <Map></Map>
+       
+                
+                <div id="map">
+                <div id="mapgiorgi"></div>
+                </div>
             </div>
         </div>
         <Chat></Chat>
@@ -30,6 +34,117 @@ export default {
         NavBar,
         Map,
         Chat
+    },
+    mounted() {
+    var database1 = firebase.database();
+        
+
+        var loadMap = {
+        // Config: {
+        //     apiKey:            "AIzaSyD4Knjz8L7b6Tbk9Ipe6lcaSMwFWPZzMfA",
+        //     authDomain:        "mapmarkers-dba7e.firebaseapp.com",
+        //     databaseURL:       "https://mapmarkers-dba7e.firebaseio.com",
+        //     projectId:         "mapmarkers-dba7e",
+        //     storageBucket:     "mapmarkers-dba7e.appspot.com",
+        //     messagingSenderId: "1047905061223",
+        //     dataBase:          "userMarkers"
+        // },
+        Pos: {
+            lat: null,
+            lng: null
+        },
+        Map: null,
+        Database: null,
+        Bounds: {},
+        UserName: 'default',
+        UserData: {},
+        CUserData: {},
+
+        GetMyPos : function () {
+
+            if (navigator.geolocation) {
+
+                navigator.geolocation.getCurrentPosition(function(position) {
+                    loadMap.Pos = {
+                        lng: position.coords.longitude,
+                        lat: position.coords.latitude
+                    };
+                    loadMap.LoadFromDatabase();
+                    loadMap.DrawMap();
+                });
+            }
+        },
+
+        LoadFromDatabase : function () {
+            // loadMap.Database = firebase.initializeApp(loadMap.Config);
+            // loadMap.Database = loadMap.Database.database();
+
+            loadMap.Database.ref("userMarkers").on("value", function(snapshoot){
+                
+                loadMap.UserData = snapshoot.val();
+                
+                for (var key in loadMap.UserData ) {
+                    if (loadMap.UserData.hasOwnProperty(key)) {
+                        loadMap.AddMarkerOn(loadMap.UserData[key].lat, loadMap.UserData[key].lng);
+                    }
+                }
+                loadMap.Map.fitBounds(loadMap.Bounds);
+            });
+
+        },
+
+        //loadMap.Config.dataBase
+        UserEvent: function () {
+            loadMap.Database.ref("/userMarkers/" + loadMap.UserName).set({
+                name: loadMap.UserName,
+                icon: "userIcon",
+                lat: loadMap.Pos.lat,
+                lng: loadMap.Pos.lng
+            });
+        },
+
+        DrawMap : function () {
+            console.log("DrawMap")
+
+            loadMap.Map = new google.maps.Map(document.getElementById("mapgiorgi"), {
+                center: {lat: loadMap.Pos.lat, lng: loadMap.Pos.lng},
+                zoom:9
+            });
+            loadMap.Bounds = new google.maps.LatLngBounds({lat: loadMap.Pos.lat, lng: loadMap.Pos.lng});
+
+            loadMap.AddMarkerOn(loadMap.Pos.lat, loadMap.Pos.lng);
+        },
+        AddMarkerOn : function (lat, lng) {
+
+            if (lat == undefined || lng == undefined) return;
+
+            var newMarker = new google.maps.Marker({position:{lat, lng}});
+            newMarker.setMap(loadMap.Map);
+            
+            google.maps.event.addListener(newMarker,'click',function(){
+                loadMap.Map.setZoom(14);
+                loadMap.Map.setCenter(newMarker.getPosition());
+                loadMap.Map.panTo(this.getPosition());
+            });
+
+            loadMap.Bounds.extend({lat: lat, lng: lng});
+        }
+        };
+        loadMap.Database = database1;
+
+        loadMap.GetMyPos();
+
+        console.log("000000000")
+
+
+
+
+
+
+
+
+
+
     },
     created: function() {
         const currentUser = firebase.auth().currentUser;
@@ -111,4 +226,8 @@ export default {
 #navbar-column {
     padding: 0;
 }
+#map, #mapgiorgi{
+    height: 100%;
+}
+
 </style>
